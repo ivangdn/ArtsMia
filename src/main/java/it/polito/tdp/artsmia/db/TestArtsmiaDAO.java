39a2ac4e0a7f798ee2548d6328893b8c0b22dfe1
Ivan Gaudino <s257971@studenti.polito.it>
2022-06-26 16:57:27 +0200

    Soluzione

--- src/main/java/it/polito/tdp/artsmia/db/TestArtsmiaDAO.java
+++ src/main/java/it/polito/tdp/artsmia/db/TestArtsmiaDAO.java
@@ -8,11 +8,11 @@ public class TestArtsmiaDAO {
 
 	public static void main(String[] args) {
 
-		ArtsmiaDAO dao = new ArtsmiaDAO();
-			
-		List<ArtObject> objects = dao.listObjects();
-		System.out.println(objects.get(0));
-		System.out.println(objects.size());
+//		ArtsmiaDAO dao = new ArtsmiaDAO();
+//			
+//		List<ArtObject> objects = dao.listObjects();
+//		System.out.println(objects.get(0));
+//		System.out.println(objects.size());
 	}
 
 }
